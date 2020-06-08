a7f453111392898ffbc6c296d693bd8e4526278b
26968986 <nggimseng@gmail.com>
2020-06-08 13:51:47 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data exploratory of video game statistics',
+    author='Gim Seng Ng',
+    license='MIT',
+)
